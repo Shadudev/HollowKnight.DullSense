5238873a79750bd3b15d303491018f48d07ecbf3
Ofir Cohen <ofirgvc@gmail.com>
2021-07-12 23:59:43 +0300

    No more sense in this world

---
+++ DullSense/DullSense.cs
@@ -0,0 +1,44 @@
+﻿using Modding;
+
+namespace DullSenseMod
+{
+    public class DullSense : Mod
+    {
+        public DullSense Instance { get; private set; }
+
+		public override void Initialize()
+		{
+			if (Instance != null)
+			{
+				LogWarn("Initialized twice... Stop that.");
+				return;
+			}
+
+			Instance = this;
+
+			RandomizerMod.GiveItemActions.ExternItemHandlers.Add(DropWorldSense);
+		}
+
+		private static bool DropWorldSense(RandomizerMod.GiveItemActions.GiveAction action,
+			string item, string location, int geo)
+        {
+			if (item == "World_Sense")
+			{
+				RandomizerMod.RandoLogger.LogItemToTracker(item, location);
+				RandomizerMod.RandomizerMod.Instance.Settings.MarkItemFound(item);
+				RandomizerMod.RandomizerMod.Instance.Settings.MarkLocationFound(location);
+				RandomizerMod.RandoLogger.UpdateHelperLog();
+
+				return true;
+			}
+
+			return false;
+        }
+
+		public override string GetVersion()
+        {
+            string ver = "0.0.1";
+            return ver;
+        }
+    }
+}
